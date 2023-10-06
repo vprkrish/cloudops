e56ff09fc729dfc8359fb1a5e61e9d5af9ab50ed
sainadh63042 <126876944+sainadh63042@users.noreply.github.com>
2023-10-06 17:19:50 +0530

    Update database.py
    rename config.json to tests/config.json

--- tests/database.py
+++ tests/database.py
@@ -2,7 +2,7 @@ import mysql.connector
 import json
 
 def load_config():
-    with open('config.json', 'r') as config_file:
+    with open('tests/config.json', 'r') as config_file:
         config_data = json.load(config_file)
     return config_data
 
